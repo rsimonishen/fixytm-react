d68fa2afd5fc742b082e9dc2d52a20c01b875588
rsimonishen <rsimonishen@gmail.com>
2025-10-04 02:33:18 +0300

    Website keyword expansion

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+console.log('FIX.YTM React is loading')
+
 import { createRoot } from 'react-dom/client'
 import { Overlay } from './Overlay'
 import fixytm from './cache-init'
-import {fetchChannels} from "./network-scripts"
+import { fetchChannels } from "./network-scripts"
 import { getRelevantGapiKey } from "./cache-scripts";
 import type {Channel} from "./related-interfaces.ts";
-
-console.log('FIX.YTM React is loading')
 
 console.log('FIX.YTM React says Hello World!')
 console.log("FIX.YTM React: Welcome to the ultimate YT Music scraper!")
